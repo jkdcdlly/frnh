8849fcf9a393aa3cbbb369403ac9e1deb860cdcf
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2025-11-25 21:35:19 +0530

    Upload Application

---
+++ src/components/ui/Icons.tsx
@@ -0,0 +1,8 @@
+import { Menu, X, Linkedin, Twitter, Facebook } from 'lucide-react';
+
+export const MenuIcon = Menu;
+export const CloseIcon = X;
+export const LinkedinIcon = Linkedin;
+export const TwitterIcon = Twitter;
+export const FacebookIcon = Facebook;
+
